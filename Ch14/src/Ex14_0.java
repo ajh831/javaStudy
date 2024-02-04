3e3722f16bbf8d03ebf2c86e8573f1e1c737fd3e
ajh831 <gnstm831@naver.com>
2024-02-04 23:11:36 +0900

    자바의 정석

--- Ch14/src/Ex14_0.java
+++ Ch14/src/Ex14_0.java
@@ -24,7 +24,7 @@ public class Ex14_0 {
 //		위의 코드를 람다식으로 짧게 줄이면 아래의 코드가 된다.
 		// 람다식(익명 객체)을 다루기 위한 참조변수의 타입은 함수형 인터페이스로 한다.
 		MyFunction_EX f = (a, b) -> a > b ? a : b; // 람다식, 익명객체
-		
+		//
 		int value = f.max(3, 5); // 함수형 인터페이스
 		System.out.println("value = " + value);
 	}
